558bcf26c88aad8b703951ac7d14c1c9d9726081
rudmoon <96547605+rudmoon@users.noreply.github.com>
2023-04-09 07:31:42 +0900

    230409_1
    230409_1

--- main.js
+++ main.js
@@ -3,17 +3,18 @@
 let slideWrapper = document.querySelector("#testimonials"),
     slides = document.querySelector(".testimonials_testimonial"),
     slide = document.querySelectorAll(".testimonial"),
+    slidei = document.querySelectorAll(".testimonial>i"),
+    slidesay = document.querySelectorAll(".testimonal__saying"),
+    slideinf = document.querySelectorAll(".client__information--name"),
+    slideprof = document.querySelectorAll(".client__information--profession"),
     slideCount = slide.length,
     currentIdx = 0,
     slideWidth = ((slideWrapper.getBoundingClientRect().width)*0.3),
     slideMargin = ((slideWrapper.getBoundingClientRect().width)-slideWidth*3)/2,
-    slideHeight = slide[0].getBoundingClientRect().height,
     slideBtn = document.querySelectorAll('.testimonials__btns button'),
     cloneBefore,
     cloneAfter;
 
-    console.log(slideWidth);
-    console.log(slideMargin);
 
 function makeingClone() {
     cloneAfter = slide[slideCount-1].cloneNode("true");
@@ -44,17 +45,63 @@ floatLeft();
 function gotoSlide(idx) {
     slides.style.left = -idx * (slideWidth+slideMargin) + 'px';
     currentIdx = idx;
+    // idx++;
+    // if(currentIdx>slideCount) {
+    //     currentIdx = 0;
+    // }
 }
 
-gotoSlide(0);
-
-for(let i=0; i<slideCount; i++) {
+// gotoslide func에 잡다한 부분 다 넣고, slideBtn[i].addEventListener('click',(ev) 이부분을 gotoslide(ev.target 이런식으로)
+for(let i=0; i<slideCount; i++) { 
     slideBtn[i].addEventListener('click',(ev) => {
         for(let j=0; j<slideCount; j++) {
             slideBtn[j].classList.remove('colored');
+            slide[j].classList.remove('colored');
+            slidei[j].classList.remove('colored');
+            slidesay[j].classList.remove('colored');
+            slideinf[j].classList.remove('colored');
+            slideprof[j].classList.remove('colored');
         }
         gotoSlide(ev.target.dataset.number-1);
         slideBtn[ev.target.dataset.number-1].classList.add('colored');
-       
+        slide[ev.target.dataset.number-1].classList.add('colored');
+        slidei[ev.target.dataset.number-1].classList.add('colored');
+        slidesay[ev.target.dataset.number-1].classList.add('colored');
+        slideinf[ev.target.dataset.number-1].classList.add('colored');
+        slideprof[ev.target.dataset.number-1].classList.add('colored');
     })
 }
+
+
+function gotoSlideZero() {
+    gotoSlide(0);
+    slideBtn[0].classList.add("colored");
+    slide[0].classList.add("colored");
+    slidei[0].classList.add("colored");
+    slidesay[0].classList.add("colored");
+    slideinf[0].classList.add("colored");
+    slideprof[0].classList.add("colored");   
+}
+
+gotoSlideZero();
+
+    let autoSlide;
+
+    function startAutoslide() {
+        autoSlide = setInterval(slideFunc,2000);
+    }
+
+    function stopAutoslide() {
+        clearInterval(autoSlide);
+    }
+
+    function slideFunc() {
+        currentIdx++;
+        if (currentIdx >= slideCount) {
+            currentIdx = 0;
+        }
+        gotoSlide(currentIdx);
+    }
+
+    startAutoslide();
+
