99d06f25f1e57e65c555cb7b8495e8e3dfeaefbd
rudmoon <96547605+rudmoon@users.noreply.github.com>
2023-04-11 04:35:17 +0900

    230411_1
    230411_1
    before responsive

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // 1. Slide Show + AutoSlide
 
-let slideWrapper = document.querySelector("#testimonials"),
+function slideShow() {
+    let slideWrapper = document.querySelector("#testimonials"),
     slides = document.querySelector(".testimonials_testimonial"),
     slide = document.querySelectorAll(".testimonial"),
     slidei = document.querySelectorAll(".testimonial>i"),
@@ -14,7 +15,6 @@ let slideWrapper = document.querySelector("#testimonials"),
     slideBtn = document.querySelectorAll('.testimonials__btns button'),
     cloneBefore,
     cloneAfter;
-
 
 function makeingClone() {
     cloneAfter = slide[slideCount-1].cloneNode("true");
@@ -58,10 +58,7 @@ function gotoSlide(idx) {
     slidesay[currentIdx].classList.add('colored');
     slideinf[currentIdx].classList.add('colored');
     slideprof[currentIdx].classList.add('colored');    
-    // idx++;
-    // if(currentIdx>slideCount) {
-    //     currentIdx = 0;
-    // }
+
 }
 function moveSlide(idx) {
     activateSlide(idx);
@@ -69,13 +66,11 @@ function moveSlide(idx) {
     startAutoslide();
 }
 
-// gotoslide func에 잡다한 부분 다 넣고, slideBtn[i].addEventListener('click',(ev) 이부분을 gotoslide(ev.target 이런식으로)
 for(let i=0; i<slideCount; i++) { 
     slideBtn[i].addEventListener("click", (ev) => {
         gotoSlide(ev.target.dataset.number - 1);
     });
 }
-
 
 function gotoSlideZero() {
     gotoSlide(0);
@@ -89,26 +84,29 @@ function gotoSlideZero() {
 
 gotoSlideZero();
 
-    let autoSlide;
+let autoSlide;
 
-    function startAutoslide() {
-        autoSlide = setInterval(slideFunc,2000);
-    }
+function startAutoslide() {
+    autoSlide = setInterval(slideFunc, 2000);
+}
 
-    function stopAutoslide() {
-        clearInterval(autoSlide);
-    }
+function stopAutoslide() {
+    clearInterval(autoSlide);
+}
 
-    function slideFunc() {
-        currentIdx++;
-        if (currentIdx >= slideCount) {
-            currentIdx = 0;
-        }
-        gotoSlide(currentIdx);
+function slideFunc() {
+    currentIdx++;
+    if (currentIdx >= slideCount) {
+    currentIdx = 0;
     }
-    // window.addEventListener("load", startAutoslide);
-    slideWrapper.addEventListener("mouseleave", startAutoslide);
-    slideWrapper.addEventListener("mouseenter", stopAutoslide);
+    gotoSlide(currentIdx);
+}
+// window.addEventListener("load", startAutoslide);
+slideWrapper.addEventListener("mouseleave", startAutoslide);
+slideWrapper.addEventListener("mouseenter", stopAutoslide);
+}
+
+
 
 
 // 2. Scrolltop시, fixed된 navbar display :none, 그 이하시 보이게(width는 100%)
@@ -125,12 +123,10 @@ function navbarFixed() {
 });
 };
 
-navbarFixed();
-
 // 3. ArrowtotheTop
 function gotoTopArrow() {
     const gototopArrow = document.querySelector('#gotoTop');
-let serviceSection = document.querySelector('#services');
+    let serviceSection = document.querySelector('#services');
     window.addEventListener('scroll', (ev) => {
         ev.preventDefault();
         if(window.scrollY>serviceSection.getBoundingClientRect().height) {
@@ -149,23 +145,24 @@ let serviceSection = document.querySelector('#services');
     })
 };
 
-gotoTopArrow();
-
 // 4. Header section 홈페이지 load되면? or scrolly가 도착하면(난 이걸로 함) load 되는것
-const navSection = document.querySelector('#nav');
-const navLogo = document.querySelector('.nav__logo');
-const navMenus = document.querySelector('.nav__menus');
-const headerleftTitle = document.querySelector('.leftContent__title');
-const headerlefScript = document.querySelector('.leftContent__script');
-const headerleftBtn = document.querySelector('.leftContent__bookBtn');
+function appearNav() {
+    const navSection = document.querySelector('#nav');
+    const navLogo = document.querySelector('.nav__logo');
+    const navMenus = document.querySelector('.nav__menus');
+    const headerleftTitle = document.querySelector('.leftContent__title');
+    const headerlefScript = document.querySelector('.leftContent__script');
+    const headerleftBtn = document.querySelector('.leftContent__bookBtn');
 
-window.addEventListener('load', () => {
-    navLogo.classList.add('show');
-    navMenus.classList.add('show');
-    headerleftTitle.classList.add('show');
-    headerlefScript.classList.add('show');
-    headerleftBtn.classList.add('show');
+    window.addEventListener('load', () => {
+        navLogo.classList.add('show');
+        navMenus.classList.add('show');
+        headerleftTitle.classList.add('show');
+        headerlefScript.classList.add('show');
+        headerleftBtn.classList.add('show');
 })
+}
+
 
 // 5. Service section 부분, scroll이 어느정도(serviceSection에 도달했을 떄로 하자) 내려왔을 때 하나씩 등장
 function appearServicesection() {
@@ -184,28 +181,121 @@ function appearServicesection() {
 appearServicesection();
 
 // 6. About section 사진부터, 다음 description 부분
-function appearAboutimg() {
+function appearAbout() {
     const aboutSection = document.querySelector("#about");
     const aboutDescription = document.querySelector('.about__description');
     const aboutImg = document.querySelectorAll('.aboutImg');
-    const toFifteen = document.querySelector('.first__left').textContent;
-    const toFifty = document.querySelector('.second__left').textContent;
+    const toFifteen = document.querySelector('.first__left');
+    const toFifty = document.querySelector('.second__left');
 
-
+-
 
 window.addEventListener('scroll',() => {
     if(window.scrollY>aboutSection.getBoundingClientRect().top) {
         aboutDescription.classList.add('show');
         for(let i=0; i<aboutImg.length; i++) {
             aboutImg[i].classList.add('show');
-        }      
+        }  
+        
+        
     }
 });
+window.onload = () => {
+    let i = 1;
+    let j = 1;
+
+    let interval1 = setInterval(() => {
+        toFifteen.innerHTML = i;
+        i++;
+        if (i > 15) {
+            clearInterval(interval1);
+        }
+    }, 200);
+    let interval2 = setInterval(() => {
+        toFifty.innerHTML = j;
+        j++;
+        if (j > 50) {
+            clearInterval(interval2);
+        }
+    }, 50);
+}
 }
 
-appearAboutimg();
-
-// 7. 
 
 
+// 7. appearFood(), 각각 class부여해서 breakfast,launch,dinner나누기)
 
+function appearFood() {
+
+    const foodsSection = document.querySelector('#foods');
+    const foodtitleContainer = document.querySelector('.foods__titleContainer');
+    const foodSubtitle = document.querySelector('.foods__subtitle');
+    const foodBtnsdiv = document.querySelector('.foods__collections');
+    const foodsCointainer = document.querySelector('.foods_choicesContainer');
+    const foodBtns = document.querySelectorAll('.collection');
+    const choice = document.querySelectorAll('.choice');
+
+    window.addEventListener('scroll',() => {
+        if(window.scrollY>foodsSection.getBoundingClientRect().top) {
+            foodtitleContainer.classList.add('show');
+            foodSubtitle.classList.add('show');
+            foodBtnsdiv.classList.add('show');
+            foodsCointainer.classList.add('show');
+        }
+    })
+
+    for(let i=0; i<foodBtns.length; i++) {
+        foodBtns[i].addEventListener('click', (ev) => {    
+            for(let k = 0; k < foodBtns.length; k++) {
+                if(k === i) {
+                    foodBtns[k].style.borderBottom = "3px solid orange";
+                } else {
+                    foodBtns[k].style.borderBottom = "1px solid gray";
+                }
+            }
+            
+            // console.log(ev.currentTarget);
+            for(let j=0; j<choice.length; j++) {
+                choice[j].classList.remove('show');
+                if(ev.currentTarget.dataset.type === choice[j].dataset.type) {
+                choice[j].classList.add('show');              
+                }
+            }            
+        });
+    }
+    for(let i=0; i<foodBtns.length; i++) {
+        foodBtns[i].addEventListener('click', (ev) => {
+            ev.currentTarget.style.opacity = 1;
+        })
+    }   
+}
+
+
+// 8. appearReservation()
+function appearReservation() {
+    const reservationSection = document.querySelector('#reservation');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY>reservationSection.getBoundingClientRect().top) {
+            reservationSection.classList.add('show');
+        }
+    })
+
+
+
+}
+
+
+function init() {
+
+    slideShow();
+    gotoTopArrow();
+    navbarFixed();
+    appearServicesection();
+    appearNav();
+    appearAbout();
+    appearFood();
+    appearReservation();
+}
+
+init();
